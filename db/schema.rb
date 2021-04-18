59d337cf4802ac17c8cef216564ed8cbe2c3eaa5
kkt9102 <kkt9102@gmail.com>
2021-04-19 08:23:50 +0900

    command scaffold(2). rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210418150928) do
+ActiveRecord::Schema.define(version: 20210418232312) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "bulletins", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
